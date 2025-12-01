34a9b91a6cfd229e375efc5129120735c1d3197f
Taman Hassan <tamanhassan1@gmail.com>
2025-12-01 21:13:44 +0100

    changes made to api

--- src/pages/Api.jsx
+++ src/pages/Api.jsx
@@ -1,12 +1,2 @@
-import Bitcoin from "../components/Bitcoin.jsx";
-
-export default function Api () {
-    return (
-        <div>
-            <h1>Api</h1>
-            <Bitcoin />
-        </div>
-    );
-}
 
 
